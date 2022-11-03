b98236e182d1262f1937b29ecc92bbbbc2e066ea
k-sang-soo <wsckm1@google.com>
2022-11-03 22:11:37 +0900

    head 태그 title 에러 수정

--- components/Seo.js
+++ components/Seo.js
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 
 export default function Seo({ title }) {
+    // title element received an array with more than 1 element as children 에러 수정
     return (
         <Head>
-            <title>{title} | Next Movies</title>
+            <title>{`${title} | Next Movies`}</title>
         </Head>
     );
 }
