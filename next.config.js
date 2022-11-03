8eda381568caf075b78847975c175408ecc2413a
k-sang-soo <wsckm1@google.com>
2022-11-03 22:07:07 +0900

    next js - redirects, rewrite 활용법

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const API_KEY = process.env.API_KEY; //env 사용 시 git ignore 잊지말자
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: false,
+    swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: '/contact', // contact 라는 주소로 유저가 이동 시
+                destination: '/form', // form 으로 이동 시킨다
+                permanent: false, // 영구적인지 아닌지에 따라서 브라우저나 검색엔진이 이 정보를 기억한다.
+            },
+        ];
+    },
+    async rewrites() {
+        // rewrites는 유저를 redirect 시키지만 주소는 변하지 않는다.
+        return [
+            {
+                source: '/api/movies',
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
