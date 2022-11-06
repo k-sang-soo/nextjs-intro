8a1269731439863040ddade64e5afe083a759f27
k-sang-soo <wsckm1@google.com>
2022-11-06 23:11:29 +0900

    서버 사이드 렌더링 구현 함수, url 파라미터 넘기기

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,29 @@
-import { useEffect, useState } from 'react';
 import Seo from '../components/Seo';
+import Link from 'next/link';
 
-export default function Home() {
-    const [movies, setMovies] = useState();
-    useEffect(() => {
-        (async () => {
-            // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
-            // const json = await response.json();
-            // 문법 줄이기
-            const { results } = await (await fetch(`/api/movies`)).json();
-            setMovies(results);
-        })();
-    }, []);
+export default function Home({ results }) {
+    // const [movies, setMovies] = useState();
+    // useEffect(() => {
+    //     (async () => {
+    //         // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+    //         // const json = await response.json();
+    //         // 문법 줄이기
+    //         const { results } = await (await fetch(`/api/movies`)).json();
+    //         setMovies(results);
+    //     })();
+    // }, []);
     return (
         <div className="container">
             <Seo title="Home" />
-            {!movies && <h4>Loading...</h4>}
-            {movies?.map((movie) => (
-                <div className="movie" key={movie.id}>
-                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    <h4>{movie.original_title}</h4>
-                </div>
+            {results?.map((movie) => (
+                <Link href={`/movies/${movie.id}`} key={movie.id}>
+                    <a>
+                        <div className="movie">
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                            <h4>{movie.original_title}</h4>
+                        </div>
+                    </a>
+                </Link>
             ))}
             <style jsx>{`
                 .container {
@@ -29,6 +32,11 @@ export default function Home() {
                     padding: 20px;
                     gap: 20px;
                 }
+
+                .movie {
+                    cursor: pointer;
+                }
+
                 .movie img {
                     max-width: 100%;
                     border-radius: 12px;
@@ -45,4 +53,17 @@ export default function Home() {
             `}</style>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    //서버에서만 작동할 수 있게 하는 함수
+    // 항상 서버사이드 랜더링을 하고 싶을 때 즉, 데이터가 유효할 때 화면이 보여지게 되는게 좋을 때
+    // api loading이 길어지면 loading이 없는 대신에 유저가 아무것도 보지 못한 채로 오래기다려야 한다는 단점이 있음
+    // api를 서버에서 불러오기 때문에 loading이 생기지 않는다.
+    const { results } = await (await fetch(`http://localhost:3000//api/movies`)).json();
+    return {
+        props: {
+            results,
+        },
+    };
 }
